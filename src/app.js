a71e4ef555bbf47c7ef267c1270d74690e9a4f35
Tanima Ranjan <tanimaranjan@gmail.com>
2019-02-09 17:08:33 -0500

    Update for selector

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import {Provider } from 'react-redux'
 import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { login, logout } from './actions/auth'
-import { setSetHabit, startSetHabit } from './actions/habits' 
+import { startSetHabit } from './actions/habits' 
 import LoadingPage from './components/LoadingPage'
 
 import '../node_modules/normalize.css/normalize.css'
@@ -12,7 +12,7 @@ import '../public/styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 
 import {firebase} from './firebase/firebase'
-import { create } from 'domain';
+
 
 const store = configureStore();
 const jsx = (
@@ -32,12 +32,12 @@ const renderApp = () => {
 
 ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
+
+
 firebase.auth().onAuthStateChanged((user) => {
     if(user) {
         store.dispatch(login(user.uid))
-        store.dispatch(startSetHabit()).then(() => {
-
-        
+        store.dispatch(startSetHabit()).then(() => {        
             renderApp()
             if (history.location.pathname === '/') {
                 history.push('/dashboard')
@@ -50,6 +50,7 @@ firebase.auth().onAuthStateChanged((user) => {
         history.push('/')
     }
 })
+
 
 /*
 store.dispatch(startSetExpenses()).then(() => {
