6cab7cbb7be2a7196892cea378506da5b56714b9
Tanima Ranjan <tanimaranjan@gmail.com>
2019-02-14 21:41:58 -0500

    Habit List update

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import selectHabits from '../selectors/habits'
+import HabitListItem from './HabitListItem';
 
-export const HabitList = () => {
-    return (
-        <div>Habit List</div>
-    )
+export const HabitList = (props) => (
+    <div className='content-container'>
+        <div className='list-header'>
+            <div className='show-for-mobile'>Habits</div>
+            <div className='show-for-desktop'>Habit</div>
+            <div className='show-for-desktop'>Days</div>
+        </div>
+        <div className='list-body'>
+        {
+            props.habits.length === 0 ? (
+                <div className='list-item list-item__message'>
+                    <span>No Habits</span>
+                </div>
+            ) : (
+                props.habits.map((habit) => {
+                    return <HabitListItem key={habit.id} {...habit} />
+                })
+            )
+        }
+        </div>
+    </div>
+)
+
+const mapStateToProps = (state) => {
+    return {
+        habits : selectHabits(state.habit)
+    }
 }
+export default connect(mapStateToProps)(HabitList)
