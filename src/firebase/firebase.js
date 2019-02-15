6cb22772f49bd87b4c3c019a255f708996e61711
Tanima Ranjan <tanimaranjan@gmail.com>
2019-02-14 21:53:28 -0500

    Formatting

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,7 @@
-import * as firebase from 'firebase'
-import moment from 'moment';
+import * as firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+
 
 // Initialize Firebase
 const config = {
